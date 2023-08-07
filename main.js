29d8da7dc68138a38c247c4b1e8f6ac23043518e
Bekzod12345 <bekzodraxmatov18@gmail.com>
2023-08-07 13:47:53 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+let string = "bekzod"
+////bu tuliq tehshradi yani true false
+console.log(string.includes).
+
+
+////bu endswith oxiridan true yok false qaytaradi
+console.log(string.endsWith())
+
+/////
+console.log(string.charCodeAt())
+
+console.log(string.charAt())
+console.log(string.codePointAt())
+
+////bu qushish  yani oxiridan qushish
+console.log(string.concat("salom"))
+
+///bu oxiridan qushadi yani lengisi qaraymiz
+console.log(string.padEnd(7,"d"))
+
+////
+console.log(string.matchAll())
+
+/////bu ichidan  indexi topadi yani raqam bilan nechanchi urinda turgani 
+console.log(string.indexOf())
+
+/////
+console.log(string.match())
+
+//// 
+
+console.log(string.normalize())
+
+
+///bu kichiklashtirish
+console.log(string.toLocaleLowerCase())
+
+
+////bu katalashtirish harflardi
+console.log(string.toLocaleUpperCase());
+///
+console.log(string.localeCompare())
+
+//////
+console.log(string.repeat())
+/////
+console.log(string.match())
+
+//////
+console.log(string.replace())
+
+/////
+
+console.log(string.trim())
+
+/////
+console.log(string.substring())
+
+//////
+
+console.log(string.padStart())
+
+
+///
+console.log(string.split.apply())
+
